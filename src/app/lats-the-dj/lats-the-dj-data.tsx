b0cef8fcdef98b50865fb52e7e7f67b25e5cc139
BriGra <client.brighti@gmail.com>
2024-10-24 13:57:23 +0100

    add lats the dj event page

---
+++ src/app/lats-the-dj/lats-the-dj-data.tsx
@@ -0,0 +1,9 @@
+const LatsTheDjData = ()=>{
+    return(
+        <div className="w-full py-[100px]">
+            <h2 className="text-blue-500 text-center">Coming Soon!!!</h2>
+        </div>
+    )
+}
+
+export default LatsTheDjData;
