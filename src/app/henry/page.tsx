0132040a75677375294a96ea61c2ad37a10901e8
BriGra <client.brighti@gmail.com>
2025-05-14 14:03:42 +0100

    add henry rsvp

---
+++ src/app/henry/page.tsx
@@ -0,0 +1,10 @@
+import GuestLayout2 from "@/components/layouts/GuestLayout2";
+import HenryData from "./henry-data";
+
+export default function Henry() {
+  return ( 
+    <GuestLayout2>
+      <HenryData />
+    </GuestLayout2>
+  );
+}
